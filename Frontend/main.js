a73e4cf358995ec1a348b4fecd8d1bd9528ebdd6
David Díaz <david.diaz.isei@gmail.com>
2013-01-16 13:50:40 -0600

    All frameworks added.

---
+++ Frontend/main.js
@@ -0,0 +1,10 @@
+Importer.importfile('Classes/Controllers/CallbacksHandler.js');
+Importer.importfile('Classes/Controllers/NavigationController.js');
+Importer.importfile('Classes/Services/Prototypes/Service.js');
+Importer.importfile('Classes/Controllers/Prototypes/ViewController.js');
+
+$(document).ready(function(){Importer.loadImports(main);});
+
+function main(){
+	console.log('main');
+};
